1880a7da308d267cd0ffffe5df78fd8152d965ad
rs-ent <rs.ent.contact@gmail.com>
2024-11-26 10:12:34 +0900

    ENV

--- src/app/firebase/firebase.js
+++ src/app/firebase/firebase.js
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDkyA_EzL99SjudTHAYTwdFi6P--bn5HI",
-  authDomain: "redslippers.firebaseapp.com",
-  projectId: "redslippers",
-  storageBucket: "redslippers.appspot.com",
-  messagingSenderId: "263516861210",
-  appId: "1:263516861210:web:5982131662655b0c32b2a3"
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomain,
+  projectId: process.env.FIREBASE_projectId,
+  storageBucket: process.env.FIREBASE_storageBucket,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+  appId: process.env.FIREBASE_appId,
 };
 
 // Initialize Firebase
