d08b2c8d9397ed6e72334676c09fe7171ec74598
rs-ent <rs.ent.contact@gmail.com>
2024-12-08 22:05:50 +0900

    investmentpoint-params

--- src/app/[artist_id]/investment-point-manager/page.js
+++ src/app/[artist_id]/investment-point-manager/page.js
@@ -1,12 +1,9 @@
-'use client';
-
 import dynamic from 'next/dynamic';
-import { useParams } from 'next/navigation';
 
 // InvestmentPointManager를 동적으로 로드하며, SSR 비활성화
 const InvestmentPointManager = dynamic(() => import('./InvestmentPointManager'), { ssr: false });
 
-export default function Page() {
-  const { artist_id } = useParams();
+export default async function Page({params}) {
+  const {artist_id} = await params;
   return <InvestmentPointManager artist_id={artist_id} />;
 }
