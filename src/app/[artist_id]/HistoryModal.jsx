72b72c4a509d746d89ae757691dbbd3ea8da113e
rs-ent <rs.ent.contact@gmail.com>
2024-12-11 15:55:25 +0900

    Modal H-full

--- src/app/[artist_id]/HistoryModal.jsx
+++ src/app/[artist_id]/HistoryModal.jsx
@@ -6,21 +6,23 @@ const HistoryModal = ({ onClose, contents }) => {
 
     return (
         <div className="fixed inset-0 z-50 flex justify-center items-center">
-            <div className="relative w-full max-w-[480px] bg-[var(--background)] shadow-lg p-6 max-h-dvh flex flex-col">
+            <div className="relative w-full max-w-[480px] max-h-dvh bg-[var(--background)] shadow-lg flex flex-col">
                 {/* 닫기 버튼 */}
                 <button
                     onClick={onClose}
-                    className="absolute top-4 right-4 bg-gray-200 hover:bg-gray-300 text-black px-3 py-1 rounded-full"
+                    className="absolute top-4 right-4 bg-gray-200 hover:bg-gray-300 text-black px-3 py-1 rounded-lg"
                     aria-label="Close Modal"
                 >
                     닫기
                 </button>
 
                 {/* 스크롤 가능한 콘텐츠 */}
-                <div className="mt-8 flex flex-col gap-1 flex-grow overflow-y-auto">
-                    {contents.map((block, index) => (
-                        <BlocksRenderer key={index} block={block} />
-                    ))}
+                <div className="mt-8 flex flex-col gap-1 min-h-dvh flex-grow overflow-y-auto">
+                    <div className="p-9">
+                        {contents.map((block, index) => (
+                            <BlocksRenderer key={index} block={block} />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
